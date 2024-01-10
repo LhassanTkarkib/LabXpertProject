041d830865778113050bf007baa5e9232d2c6716
Ryukiro <hassanth355@gmail.com>
2024-01-10 12:47:36 +0100

    working on yaml file

---
+++ src/main/java/org/apache/maven/archetypes/labxpertproject/entitiy/Utilisateur.java
@@ -0,0 +1,15 @@
+package org.apache.maven.archetypes.labxpertproject.entitiy;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "utilisateur")
+public class Utilisateur {
+
+    @
+
+
+}
